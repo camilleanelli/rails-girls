81a9991b55432584f4d35602d94e9cc6579bb0cf
camilleanelli <camilleanelli32@gmail.com>
2015-07-06 13:30:27 +0200

    add devise and model user

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.2'
 # Use sqlite3 as the database for Active Record
 
 # Use SCSS for stylesheets
+gem 'devise'
 
 gem 'carrierwave'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :comments
   get 'pages/info'
 
   resources :ideas
 
-  root :to => redirect('/ideas')
+  root to: "ideas#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
